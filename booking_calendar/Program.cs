53c4e599dc43ff61da1d081e1c9cbed777c6f8cd
Boston Stamm <104592830+BostonStamm@users.noreply.github.com>
2026-04-01 19:17:41 -0600

    Made an error page for Antiforgery

--- booking_calendar/Program.cs
+++ booking_calendar/Program.cs
@@ -1,4 +1,6 @@
 using booking_calendar.Components;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,10 +17,30 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
+
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        if (exception is AntiforgeryValidationException)
+        {
+            context.Response.Redirect("/antiforgery-error");
+            return;
+        }
+
+        context.Response.Redirect("/error");
+    });
+});
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
